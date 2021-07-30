8359a8974eae7ba02a3ecd731598c7cdfe126fe1
Victor Oleinikov <victor.oleinikov@gmail.com>
2021-07-30 14:44:49 -0700

    Add zipcode empty validation to uvi_service

--- app/services/uvi_service.rb
+++ app/services/uvi_service.rb
@@ -2,6 +2,7 @@ class UviService
 
   class << self
     def get_uvi zipcode
+      raise ZipEmpty unless zipcode
       raise InvalidZipFormat unless zipcode_format_valid? zipcode
       OpenWeather.get_uvi_by_zip zipcode
     end
@@ -21,6 +22,12 @@ class UviService
   class InvalidZipFormat < CustomError
     def custom_msg
       "Please enter a valid, five-digit zipcode (e.g. 12345)."
+    end
+  end
+
+  class ZipEmpty < CustomError
+    def custom_msg
+      "Zipcode cannot be empty.  Please enter a valid, five-digit zipcode (e.g. 12345)."
     end
   end
 end
